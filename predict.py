f3954101731826b40f9af3557f7d95ca1658f16e
Gregory Wieber <gregorywieber@gmail.com>
2024-01-09 14:00:55 -0800

    seed everything

--- predict.py
+++ predict.py
@@ -276,6 +276,14 @@ class Predictor(BasePredictor):
 
         print(f"Using seed: {seed}")
 
+        os.environ['PYTHONHASHSEED'] = str(seed)
+        np.random.seed(seed)
+        torch.manual_seed(seed)
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+
         prompt = prompt.strip().splitlines()
         if len(prompt) == 1:
             print("Found 1 prompt:")
