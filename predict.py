3dae6a0828412f1ea84de5f4e553bae69c7b317e
Gregory Wieber <gregorywieber@gmail.com>
2024-01-10 01:49:05 -0800

    Fixes issue where controlNet is non-deterministic  (#7)
    * Pass generator into prepare_latents call
    
    * seed everything
    
    * Update cog.yaml
    
    * Update predict.py

--- predict.py
+++ predict.py
@@ -276,6 +276,13 @@ class Predictor(BasePredictor):
 
         print(f"Using seed: {seed}")
 
+        os.environ['PYTHONHASHSEED'] = str(seed)
+        torch.manual_seed(seed)
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+
         prompt = prompt.strip().splitlines()
         if len(prompt) == 1:
             print("Found 1 prompt:")
