2f64d13813044e73ac5cdc515cd5e7f37cf57fc7
Paul <fofr@users.noreply.github.com>
2024-01-10 09:56:34 +0000

    Remove unnecessary seed setting lines

--- predict.py
+++ predict.py
@@ -278,10 +278,8 @@ class Predictor(BasePredictor):
 
         os.environ['PYTHONHASHSEED'] = str(seed)
         torch.manual_seed(seed)
-        torch.cuda.manual_seed(seed)
         torch.cuda.manual_seed_all(seed)
         torch.backends.cudnn.deterministic = True
-        torch.backends.cudnn.benchmark = False
 
         prompt = prompt.strip().splitlines()
         if len(prompt) == 1:
